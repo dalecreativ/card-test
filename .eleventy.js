921a365edd010b148300ac2b96cace8c225efb07
dalecreativ <dbhuds@gmail.com>
2021-12-01 13:14:49 +0000

    Add dayjs as dependency to handle timeFromNow with publish date from today Add filter to format the publish date as per designs Update some more spacing - can't target first-child in card__content to remove top  margin as we're playing with order within content

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
+const dayjs = require("dayjs");
+var relativeTime = require("dayjs/plugin/relativeTime");
+
 module.exports = function(config) {
   config.addPassthroughCopy({
     "./src/_includes/css/styles.css": "styles.css",
   });
+
+  config.addFilter("timeFromNow", function(value) {
+    dayjs.extend(relativeTime);
+    let date = new Date(value);
+    return dayjs(date).fromNow();
+  });
+
   return {
     dir: {
       input: "./src",
